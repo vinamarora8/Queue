d32f6a1b11e534313b32f3375449e239fffc50c0
ven0m <vinam.arora8@gmail.com>
2016-04-24 10:54:54 +0530

    Created the basic queue class for integers. updated gitignore

--- queue.cpp
+++ queue.cpp
@@ -3,7 +3,48 @@ using namespace std;
 
 class queue{
 private:
-	
+	int size;
+	int *first, *last;
 public:
-	
+	queue(int l)
+	{
+		size = l;
+		first = new int[l];
+		last = first;
+	}
+
+	void enqueue(int element)
+	{
+		*last = element;
+		last++;
+	}
+
+	int dequeue()
+	{
+		int ret = *first;
+		first++;
+		return ret;
+	}
+
+	int peek()
+	{
+		return *first;
+	}
 };
+
+int main()
+{
+	queue q(5);
+	int a;
+	for (int i=0;i<10; i++)
+	{
+		cin >> a;
+		q.enqueue(a);
+		cout << "enqueue operation successfull" << endl;
+	}
+
+	for (int i=0;i<10;i++)
+	{
+		cout << q.dequeue() << endl;
+	}
+}
